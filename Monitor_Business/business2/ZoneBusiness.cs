c8dff1499b546adfed0f5422670790440cde4380
kingster-code <daniel.reis.27997@gmail.com>
2021-05-30 18:52:02 +0100

    MOnitor ZoneBusiness cleanup

--- Monitor_Business/business2/ZoneBusiness.cs
+++ Monitor_Business/business2/ZoneBusiness.cs
@@ -11,18 +11,14 @@ namespace Monitor_Business.business2
 {
     public class ZoneBusiness : IZoneBusiness
     {
-        private readonly IZoneRegistrator _zoneRegister;
-        private readonly IZoneAtmRegistrator _zoneAtmReg;
         private readonly IInfoGetter _datagetter;
 
-        public ZoneBusiness(IZoneRegistrator zoneRegister, IZoneAtmRegistrator zoneAtmReg, IInfoGetter datagetter)
+        public ZoneBusiness(IInfoGetter datagetter)
         {
-            _zoneRegister = zoneRegister;
-            _zoneAtmReg = zoneAtmReg;
             _datagetter = datagetter;
         }
 
-       
+
 
         public IEnumerable<RoberyDTO> GetZoneNumberRobery()
         {
@@ -33,8 +29,6 @@ namespace Monitor_Business.business2
                 ZonaName = item.Name,
                 NumberSteels = _datagetter.QueryZoneRobery(item.ZoneId)
             });
-
-
         }
 
 
@@ -42,28 +36,21 @@ namespace Monitor_Business.business2
         public List<AtmData> GetAtmZone24Hour(int zoneid)
         {
             return _datagetter.QueryAtmosphereZoneList(zoneid).Where(s => (s.Date.AddDays(1) >= DateTime.Now)).OrderBy(a => a.Date).ToList();
-
-
-
         }
 
         public List<AtmMesDTO> GetAtmZoneLastMonth(int zoneid)
         {
-            
-            return _datagetter.QueryAtmosphereZoneList(zoneid).Where(s => s.Date.AddMonths(1) >= DateTime.Now).OrderBy(a => a.Date).GroupBy(s=>s.Date.Day).Select(item=> new AtmMesDTO
+            return _datagetter.QueryAtmosphereZoneList(zoneid).Where(s => s.Date.AddMonths(1) >= DateTime.Now).OrderBy(a => a.Date).GroupBy(s => s.Date.Day).Select(item => new AtmMesDTO
             {
                 Co = _datagetter.QueryCoDay(new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key)),
                 Co2 = _datagetter.QueryCo2Day(new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key)),
-                ano = new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key),
                 Hc = _datagetter.QueryHcDay(new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key)),
                 MP = _datagetter.QueryMpDay(new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key)),
                 NOx = _datagetter.QueryNOxDay(new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key)),
                 Rcho = _datagetter.QueryRchoDay(new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key)),
                 SOx = _datagetter.QuerySOxDay(new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key)),
-                
+                ano = new DateTime(DateTime.Now.Year, DateTime.Now.Month, item.Key)
             }).ToList();
-
-
         }
 
         public List<AtmMesDTO> GetAtmZoneLast3Months()
@@ -71,44 +58,57 @@ namespace Monitor_Business.business2
             var data = new DateTime(DateTime.Now.Year, DateTime.Now.AddMonths(0).Month, 01);
             var aux = _datagetter.QueryAtmosphereList().Where(s => s.Date >= data).OrderBy(a => a.Date).ToList();
             var ano = new List<AtmMesDTO>();
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < 1; i++)
+            {
                 var a = new AtmMesDTO
                 {
-                    Co = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Select(s => s.CO).Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
-                    Co2 = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Select(s => s.CO2).Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
-                    ano = data.AddMonths(i),
-                    Hc = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Select(s => s.HC).Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
-                    MP = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Select(s => s.MP).Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
-                    NOx = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Select(s => s.NOx).Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
-                    Rcho = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Select(s => s.Rcho).Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
-                    SOx = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Select(s => s.SOx).Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
+                    Co = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month)
+                             .Select(s => s.CO)
+                             .Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
+                    Co2 = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month)
+                              .Select(s => s.CO2)
+                              .Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
+                    Hc = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month)
+                             .Select(s => s.HC)
+                             .Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
+                    MP = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month)
+                             .Select(s => s.MP)
+                             .Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
+                    NOx = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month)
+                              .Select(s => s.NOx)
+                              .Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
+                    Rcho = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month)
+                               .Select(s => s.Rcho)
+                               .Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
+                    SOx = (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month)
+                              .Select(s => s.SOx)
+                              .Sum()) / (aux.Where(s => s.Date.Year == data.Year && s.Date.Month == data.AddMonths(i).Month).Count()),
                     Zone = "",
+                    ano = data.AddMonths(i)
                 };
                 ano.Add(a);
-
             }
-
             return ano;
         }
 
 
-      
 
-        public List<NumberVehicleZoneDTO> NumberVehicleZone(){
 
-               var veiculos = _datagetter.QueryVehicleDistinctZoneData().ToList();
+        public List<NumberVehicleZoneDTO> NumberVehicleZone()
+        {
 
-               var vehiclezoneDTO = veiculos.AsParallel().Select(item => new VehicleZoneDTO
-                    {
+            var veiculos = _datagetter.QueryVehicleDistinctZoneData().ToList();
 
-                        VehicleID = item.VehicleId,
-                        Zone = _datagetter.QueryZoneAreaVehicle(item.VehicleId)
-                    });
-                    return vehiclezoneDTO.AsParallel().GroupBy(s => s.Zone).OrderBy(s=>s.Key).Select(item => new NumberVehicleZoneDTO
-                    {
-                        Zone = item.Key,
-                        Vehicles = item.Count(),
-                    }).ToList();
+            var vehiclezoneDTO = veiculos.AsParallel().Select(item => new VehicleZoneDTO
+            {
+                VehicleID = item.VehicleId,
+                Zone = _datagetter.QueryZoneAreaVehicle(item.VehicleId)
+            });
+            return vehiclezoneDTO.AsParallel().GroupBy(s => s.Zone).OrderBy(s => s.Key).Select(item => new NumberVehicleZoneDTO
+            {
+                Zone = item.Key,
+                Vehicles = item.Count(),
+            }).ToList();
 
 
 
@@ -123,8 +123,6 @@ namespace Monitor_Business.business2
                 Zone = item.ZoneId.ToString(),
                 Vehicles = _datagetter.QueryParkZone(item.ZoneId),
             }).ToList();
-
-
         }
 
 
@@ -134,11 +132,11 @@ namespace Monitor_Business.business2
         public List<AtmMesDTO> ZoneAtmMonth(DateTime ano, int zoneId)
         {
             List<Zone> lista = _datagetter.QueryGetZoneList();
-            var aux = _datagetter.QueryAtmosphereZoneList(zoneId).Where(s => s.Date.Year == ano.Year && s.Date.Month==ano.Month);
+            var aux = _datagetter.QueryAtmosphereZoneList(zoneId).Where(s => s.Date.Year == ano.Year && s.Date.Month == ano.Month);
 
             return lista.Select(item => new AtmMesDTO
             {
-                Co = (aux.Where(s=>s.Sensor.Zone.ZoneId==item.ZoneId).Select(s=>s.CO).Sum())/(aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Count()),
+                Co = (aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Select(s => s.CO).Sum()) / (aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Count()),
                 Co2 = (aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Select(s => s.CO2).Sum()) / (aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Count()),
                 ano = ano,
                 Hc = (aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Select(s => s.HC).Sum()) / (aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Count()),
@@ -148,25 +146,33 @@ namespace Monitor_Business.business2
                 SOx = (aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Select(s => s.SOx).Sum()) / (aux.Where(s => s.Sensor.Zone.ZoneId == item.ZoneId).Count()),
                 Zone = item.Name,
             }).ToList();
-           
-
         }
 
+        /// <summary>
+        /// Gets the number of zones for display in the Monitor.
+        /// </summary>
+        /// <returns></returns>
         public int GetNumberZones()
         {
             return _datagetter.QueryNumberZones();
         }
+
+        /// <summary>
+        /// Gets the number of roberie per zone for display in the Monitor.
+        /// </summary>
+        /// <returns></returns>
         public int GetNumberRoberys()
         {
             return _datagetter.QueryNumberRoberys();
         }
 
-
+        /// <summary>
+        /// Gets the number of sensors for display in the Monitor
+        /// </summary>
+        /// <returns></returns>
         public int GetNumberSensores()
         {
             return _datagetter.QueryNumberSensores();
         }
-
-
     }
 }
